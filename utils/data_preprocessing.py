73d290a1438a31b4cc7a6ac6174f6b2a017fbe33
Kutlualp Tazefidan <ktazefidan@hotmail.com>
2023-09-14 15:09:16 +0200

    takeout data preprocessing pipeline

--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -1,8 +1,8 @@
-import joblib
+# import joblib
 
-# Load your pre-processing pipeline
-pipeline = joblib.load('your_pipeline.joblib')
+# # Load your pre-processing pipeline
+# pipeline = joblib.load('your_pipeline.joblib')
 
-def preprocess_data(input_data):
-    preprocessed_data = pipeline.transform(input_data)
-    return preprocessed_data
+# def preprocess_data(input_data):
+#     preprocessed_data = pipeline.transform(input_data)
+#     return preprocessed_data
