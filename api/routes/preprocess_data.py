e49e94990151e5d71be8b3db836e74cf35875f60
Kutlualp Tazefidan <ktazefidan@hotmail.com>
2023-09-15 01:02:20 +0200

    Integrate classes

--- api/routes/preprocess_data.py
+++ api/routes/preprocess_data.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from schemas.flight_info import FlightInfo
 from utils.preprocessing.pipeline_1.preprocessing_pipeline import load_preprocessing_pipeline
+from utils.preprocessing.pipeline_1.custom_classes import ColumnNameFixer, CalculateDistance, CustomFeaturesAdder, DropColumns, LabelEncoderTransformer
 
 router = APIRouter()
 
@@ -8,7 +9,7 @@ router = APIRouter()
 async def preprocess_data(input_data: FlightInfo):
     try:
         preprocessing_pipeline = load_preprocessing_pipeline()
-        preprocessed_data = preprocessing_pipeline.transform(input_data)
-        return {"preprocessed": preprocessing_pipeline}
+        # preprocessed_data = preprocessing_pipeline.transform(input_data)
+        return {"preprocessed": input_data}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- utils/preprocessing/pipeline_1/preprocessing_pipeline.py
+++ utils/preprocessing/pipeline_1/preprocessing_pipeline.py
@@ -1,7 +1,20 @@
 import joblib
+import logging  # Import the logging module
 from config import settings  # Import application settings
+from utils.preprocessing.pipeline_1.custom_classes import ColumnNameFixer, CalculateDistance, CustomFeaturesAdder, DropColumns, LabelEncoderTransformer
+
+# Configure the logging settings
+logging.basicConfig(level=logging.ERROR)  # Set the logging level to DEBUG
 
 def load_preprocessing_pipeline():
-    # Load the preprocessing pipeline from the joblib file
-    pipeline = joblib.load(settings.PREPROCESSING_PIPELINE_PATH)
-    return pipeline
+    try:
+        # Add a print statement to check the file path
+        print("Loading preprocessing pipeline from:", settings.PREPROCESSING_PIPELINE_PATH)
+
+        # Load the preprocessing pipeline from the joblib file
+        pipeline = joblib.load(settings.PREPROCESSING_PIPELINE_PATH)
+        return pipeline
+    except Exception as e:
+        # Log the error message if an exception is raised
+        logging.error(f"Error in load_preprocessing_pipeline: {str(e)}")
+        raise
