619792ee0b6adfb846def8982ffc7a4e5fb39e6c
Kutlualp Tazefidan <ktazefidan@hotmail.com>
2023-09-14 21:25:59 +0200

    Fix prefix

--- api/routes/preprocess_data.py
+++ api/routes/preprocess_data.py
@@ -7,9 +7,8 @@ router = APIRouter()
 @router.post('/preprocessdata')
 async def preprocess_data(input_data: FlightInfo):
     try:
+        # preprocessing_pipeline = load_preprocessing_pipeline()
+        # preprocessed_data = preprocessing_pipeline.transform(input_data)
         return {"preprocessed": input_data}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-        # preprocessing_pipeline = load_preprocessing_pipeline()
-        # preprocessed_data = preprocessing_pipeline.transform(input_data)
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # Libraries
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi.openapi.models import OAuthFlowPassword as OAuthFlowPasswordModel
 from api.routes import predict_flight_delay,preprocess_data
 from config import settings  # Import application settings
 
 app = FastAPI()
+app.debug = True
 
 # Configure CORS
 app.add_middleware(
@@ -20,5 +23,10 @@ def info_message():
     return "API to estimate flight delays, available routes: /prediction /preprocessdata"
 
 # Include the prediction router
-app.include_router(predict_flight_delay.router, prefix="/predict")
-app.include_router(preprocess_data.router, prefix="/preprocessdata")
+app.include_router(predict_flight_delay.router)
+app.include_router(preprocess_data.router)
+
+# Enable automatic documentation generation
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=10000)
