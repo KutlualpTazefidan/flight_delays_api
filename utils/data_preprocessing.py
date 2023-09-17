a0a654c0db39a01f5914296b6abbfc4a4049a4d3
Kutlualp Tazefidan <ktazefidan@hotmail.com>
2023-09-17 23:51:43 +0200

    remove files

--- utils/data_preprocessing.py
+++
@@ -1,8 +0,0 @@
-# import joblib
-
-# # Load your pre-processing pipeline
-# pipeline = joblib.load('your_pipeline.joblib')
-
-# def preprocess_data(input_data):
-#     preprocessed_data = pipeline.transform(input_data)
-#     return preprocessed_data
