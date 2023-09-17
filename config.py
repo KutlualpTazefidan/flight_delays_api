b17612e422ca88bbd58939c3a0bb2bfb2b112e3e
Kutlualp Tazefidan <ktazefidan@hotmail.com>
2023-09-17 23:52:28 +0200

    update model path

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     ALLOWED_ORIGINS: list = ["*"]
-    MODEL_PATH: str = "models/api_test_logistic_regression_model.joblib"
-    PREPROCESSING_PIPELINE_PATH:str = "utils/preprocessing/pipeline_1/preprocessing_pipeline_imported_classes.joblib"
-
+    MODEL_PATH: str = "pipelines_for_ml/ml_models/api_test_logistic_regression_model.joblib"
+    # PREPROCESSING_PIPELINE_PATH:str = "utils/preprocessing/pipeline_1/preprocessing_pipeline_imported_classes.joblib"
 settings = Settings()
